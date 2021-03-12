ec58ec2869b6c4c3ef887491782771a4ce38508e
Rahul Rathore <rahulrathore576@gmail.com>
2021-03-12 19:29:50 +0530

    fix: start ui from packages folder

---
+++ packages/core-angular/src/app/index.ts
@@ -0,0 +1,6 @@
+export { environment } from './../environments/environment';
+export { AppModule } from './app.module';
+
+export * from './@core';
+export * from './@shared';
+export * from './@theme';
